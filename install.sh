d05a26879f63c0222cd615e511e80d17c4c23114
Peri <peri@srdi.org>
2023-02-09 23:34:17 +0000

    [install.sh] Preserve file attributes on copy.

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -x
 
 mkdir "$1"
-cp CSRBfsTweaks.so run.sh "$1/"
+cp -p CSRBfsTweaks.so run.sh "$1/"
 
 #cat /lib/x86_64-linux-gnu/libc.so.6 > "$1/libc.so.6"
 #cat /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 > "$1/ld-linux-x86-64.so.2"
 #cat /lib/x86_64-linux-gnu/libpthread.so.0 > "$1/libpthread.so.0"
 #cat /lib/x86_64-linux-gnu/libdl.so.2 > "$1/libdl.so.2"
+
